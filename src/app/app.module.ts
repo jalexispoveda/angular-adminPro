50fb85e45f81ff531dcae334f01cd670803fec6e
jalexispoveda010 <jalexispoveda010@gmail.com>
2021-01-15 19:07:44 -0500

    Adding routes (Routes per module)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { AuthModule } from './auth/auth.module';
 import { NgModule } from '@angular/core';
 
+//Modulos
+import { PagesModule } from './pages/pages.module';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { PagesComponent } from './pages/pages.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    BreadcrumsComponent,
-    SidebarComponent,
-    HeaderComponent,
-    ProgressComponent,
-    Grafica1Component,
-    PagesComponent
+    AppComponent,       
+    NopagefoundComponent,    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
